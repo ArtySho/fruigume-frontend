105932d18a2e745554dd0f0ffb727ecdeff32f85
Altod69 <kelvinfreeze@hotmail.com>
2019-03-19 14:27:44 +0100

    new branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ListPanierComponent } from './list-panier/list-panier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListPanierComponent
   ],
   imports: [
     BrowserModule
