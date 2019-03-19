5fb3a5ab84f508666b07e377ea10d9418453c4d4
johan campion <johan-externe.campion@edf.fr>
2019-03-19 15:14:43 +0100

    feat: service de validation des carts

--- src/app/backend-service.service.ts
+++ src/app/backend-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private cartUrl = 'localhost:8080/cart';
+
+
+  validateCart(id): Observable<any> {
+    return this.http.get('localhost:8080/cart/validate/' + id);
+  }
+
+
 }
